443304b97424f6b09eb4b0e8ab10d8e8e2251341
Ammar <ammar.madni@yahoo.co.uk>
2022-02-19 20:26:25 +0000

    create a guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("\nGuess a number between 1 and 100.\n");
+
+    let target_number = rand::thread_rng().gen_range(1..101);
+    println!("The target number is: {}", target_number);
+
+    println!("Input your guess:");
+
+    let mut guess = String::new();
+
+    loop {
+        guess.clear();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+
+        let guess: u32 = guess.trim().parse().expect("Must be a number!");
+        
+        println!("Your guess: {}", guess);
+
+        match guess.cmp(&target_number) {
+            Ordering::Less => println!("Too small."),
+            Ordering::Greater => println!("Too big."),
+            Ordering::Equal => {
+                println!("Well done! Your guess was correct.");
+                break;
+            }
+        }
+    }
 }
